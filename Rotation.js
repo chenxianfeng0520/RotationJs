cd18f1c915a80020b0ec3a2e081aec892367298d
chenxianfeng0520 <chenxianfeng0520@gmail.com>
2020-12-15 14:08:50 +0800

    点击切换按钮的时候，自动播放暂停，并且在不点击按钮的8秒后恢复正常

--- Rotation.js
+++ Rotation.js
@@ -18,7 +18,7 @@
                 return this;
             }
             var r = $(a), m = $(a + " ul"), e = $(a + " ul li"), l = e.length;
-            r.css({ width: w ? (w + "px") : "100%", height: h ?? 200 + "px" });
+            r.css({ width: w ? (w + "px") : "100%", height: h ?? 300 + "px" });
             m.css({ width: l * 100 + "%" });
             e.css({ width: 100 / l + "%" });
             if (l == 1) {
@@ -29,30 +29,43 @@
             for (let index = 0; index < l; index++) {
                 u.append("<div></div>");
             }
-            if (b) {
-                var thar = this;
+            var i = true;
+            if (!b) {
                 setInterval(function () {
-                    thar.printLog(s);
-                    g();
-                    s++;
-                    s == l && (s = 0);
+                    console.log('i:', i);
+                    if (i) {
+                        g();
+                        s++;
+                        s == l && (s = 0);
+                    }
                 }, c ?? 2000);
             }
             r.append("<div class='Rotation-left'></div>");
             r.append("<div class='Rotation-right'></div>");
             var left = r.children(".Rotation-left");
             var right = r.children(".Rotation-right");
-            left.on("click",function(){
+            var t;
+            left.on("click", function () {
+                clearTimeout(t);
+                i = false;
                 g();
                 s--;
-                s == -1 && (s = (l-1));
+                s == -1 && (s = (l - 1));
+                t = setTimeout(function () {
+                    i = true;
+                }, 8000);
             });
-            right.on("click",function(){
+            right.on("click", function () {
+                clearTimeout(t);
+                i = false;
                 g();
                 s++;
                 s == l && (s = 0);
+                t = setTimeout(function () {
+                    i = true;
+                }, 8000);
             });
-            var g = function (){
+            var g = function () {
                 e.eq(s).addClass("active").siblings().removeClass("active");
                 u.children("div").eq(s).addClass("active").siblings().removeClass("active");
                 m.css("left", w ? (-s * w + "px") : (-s * 100 + "%"));
@@ -66,10 +79,6 @@
     A.fn.init.prototype = A.fn
     window.Rotation = A
 })(window);
-$(function () {
-    Rotation("#Rotation1");
-})
-
 
 
 

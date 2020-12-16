26f93babf056d507eb8596d51fefdb658989ac9a
chenxianfeng0520 <chenxianfeng0520@gmail.com>
2020-12-16 09:21:19 +0800

    完善案例

--- Rotation.js
+++ Rotation.js
@@ -5,20 +5,24 @@
     var s = 0, p = true;
     A.fn = A.prototype = {
         init: function (a, b, c, w, h) {
+            var error = false;
             if (!document.body) {
                 console.error('请在DOM加载完成后调用Rotation()');
-                return this;
+                error = true;
             }
             if (!window.$) {
                 console.error('缺少Jquery.js文件支持');
-                return this;
+                error = true;
             }
             if (typeof a !== "string") {
                 console.error('Rotation()参数类型是否是string');
+                error = true;
+            }
+            if (error) {
                 return this;
             }
             var r = $(a), m = $(a + " ul"), e = $(a + " ul li"), l = e.length;
-            r.css({ width: w ? (w + "px") : "100%", height: h ?? 300 + "px" });
+            r.css({ width: w ? (w + "px") : "100%", height: h ?? 800 + "px" });
             m.css({ width: l * 100 + "%" });
             e.css({ width: 100 / l + "%" });
             if (l == 1) {
@@ -53,7 +57,7 @@
                         s++;
                         s == l && (s = 0);
                     }
-                }, c ?? 5000);
+                }, c ?? 2500);
             }
             r.append("<div class='Rotation-left'></div>");
             r.append("<div class='Rotation-right'></div>");
